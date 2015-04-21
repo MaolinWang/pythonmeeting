d9757366b45df2d6926287e19040f13f1d12c6f3
Maolin Wang <wangml1240@163.com>
2015-04-21 13:19:14 +0100

    Add problem 22

---
+++ Problem22.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def problem22():
+    names = pd.read_csv('D:/software/ipython_notebook/names.txt',header=None, keep_default_na=False)
+    namesorted=names.iloc[0].order()
+
+    namesscore=0
+    for i in range(len(namesorted)):
+        namescore=sum(ord(x)-64 for x in namesorted.iloc[i])
+        namesscore += namescore*(i+1)
+
+    print (namesscore)
+
+problem22()
